c2599c267b6bd878494049086bd0d517c68ca6e8
Canberk Çakmak <canberkcakmak@ogoodigital.com>
2024-03-10 23:32:01 +0300

    Initial Commit

---
+++ BundleiOSCase/BundleiOSCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BundleiOSCaseApp.swift
+//  BundleiOSCase
+//
+//  Created by Canberk Çakmak on 10.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct BundleiOSCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
